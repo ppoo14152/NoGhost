e9c26f102e350003800ba4eb3e182bffe94ce475
Reikko <cris.toluca@hotmail.com>
2015-04-24 11:25:46 -0500

    Agregando la clase2

---
+++ proyecto/Clase2.java
@@ -0,0 +1,19 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Clase2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Clase2 extends Actor
+{
+    /**
+     * Act - do whatever the Clase2 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}

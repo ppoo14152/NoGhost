c96d76ac110b92a9599f2a8f7789ba48e9427c76
Reikko <cris.toluca@hotmail.com>
2015-05-24 21:45:03 -0500

    Agregando sonido al intro y quitando sonidos inecesarios

--- proyecto/Menu.java
+++ proyecto/Menu.java
@@ -12,6 +12,7 @@ public class Menu extends World
     private Actor jugar;//creando actor de boton jugar
     private Actor credito;//creando actor de boton credito
     private Actor record;//creando actor de boton records
+    private GreenfootSound intro=new GreenfootSound("Intro.mp3");
     
     /**
      * Constructor for objects of class Mundo.
@@ -39,22 +40,27 @@ public class Menu extends World
     
     public void act()
     {
+        intro.play();
         if(Greenfoot.getMouseInfo()!=null)
         {
             if(Greenfoot.getMouseInfo().getButton()==1 && Greenfoot.getMouseInfo().getActor() == ayuda)
             {
+                this.stopped();
                 Greenfoot.setWorld(new Ayuda());//manda al escenario de ayuda
             }
             if(Greenfoot.getMouseInfo().getButton()==1 && Greenfoot.getMouseInfo().getActor() == jugar)
             {
+                this.stopped();
                 Greenfoot.setWorld(new Casa());//manda al escenario de casa
             }
              if(Greenfoot.getMouseInfo().getButton()==1 && Greenfoot.getMouseInfo().getActor() == credito)
             {
+                this.stopped();
                 Greenfoot.setWorld(new Credito());//manda al escenario de credito
             }
             if(Greenfoot.getMouseInfo().getButton()==1 && Greenfoot.getMouseInfo().getActor() == record)
             {
+                this.stopped();
                 Greenfoot.setWorld(new Records());//manda al escenario de records
             }
             
@@ -83,6 +89,26 @@ public class Menu extends World
         
         CasperMenu cM = new CasperMenu(620, 200, 1);//se agrega y se le da el movimiento a el protagonista
         addObject(cM, 620, 200);
+    }
+    
+    /**
+     * Metodo para iniciar el juego al darle play 
+     */
+    public void started()
+    {
+        super.started();
+        intro.play();
+
+    }
+
+    /**
+     * metodo para detener el juego.
+     */
+    public void stopped() 
+    {
+        super.stopped();
+        intro.pause();
+
     }
     
 }
